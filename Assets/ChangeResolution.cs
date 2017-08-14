a3e0f24e1c627b73b6165213f398eb2a68c6e47f
Javier Bullrich <javierbullrich@gmail.com>
2017-08-14 11:25:31 -0300

    Fixed a bug with the minified console

---
+++ Assets/ChangeResolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+// By @Bullrich
+namespace Test
+{
+    public class ChangeResolution : MonoBehaviour
+    {
+        [SerializeField] public resolution[] resolutions;
+
+        public int resolutionIndex;
+        public bool applyResolution;
+
+        [Serializable]
+        public struct resolution
+        {
+            public int height, width;
+        }
+
+        private void Update()
+        {
+            if (!applyResolution) return;
+            updateResolution(resolutionIndex);
+            applyResolution = false;
+        }
+
+        private void updateResolution(int index)
+        {
+            if (index > resolutions.Length - 1)
+                return;
+
+            Screen.SetResolution(resolutions[index].width, resolutions[index].height, true);
+            print(string.Format("Changed resolution to {0}x{1}", resolutions[index].height, resolutions[index].width));
+        }
+    }
+}
